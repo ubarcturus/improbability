6bceca6ddefd92292e60ed6a247fec14e53cd578
Philipp Weiermann <philipp.weiermann@bluewin.ch>
2021-03-16 17:13:51 +0100

    Project file configured

--- Improbability/Startup.cs
+++ Improbability/Startup.cs
@@ -10,8 +10,11 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
 namespace Improbability
 {
@@ -34,6 +37,52 @@ namespace Improbability
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Improbability API",
+                    Version = "v1",
+                    Description = "lirum larum Loeffelstiel",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Philipp Weiermann",
+                        Email = "philipp.weiermann@bluewin.ch"
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "MIT License",
+                        Url = new Uri("https://opensource.org/licenses/MIT")
+                    }
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath, true);
+
+                c.AddSecurityDefinition("apiKey",
+                    new OpenApiSecurityScheme
+                    {
+                        Type = SecuritySchemeType.ApiKey,
+                        In = ParameterLocation.Header,
+                        Name = "Authorization",
+                        Description = "Please enter your API-Key. Example: `Key 5BLBAI3PGNUPVO5GFKMUSSPC6KCAE2M7`"
+                    });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "apiKey" }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+
+                c.EnableAnnotations();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,9 +98,10 @@ namespace Improbability
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
@@ -59,9 +109,23 @@ namespace Improbability
             app.UseAuthentication();
             app.UseAuthorization();
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Improbability v1");
+                c.DisplayRequestDuration();
+                c.EnableDeepLinking();
+            });
+            app.UseReDoc(c =>
+            {
+                c.RoutePrefix = "docs";
+                c.ExpandResponses("200,201");
+            });
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
