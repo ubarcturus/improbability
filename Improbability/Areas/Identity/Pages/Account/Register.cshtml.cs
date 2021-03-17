d2a5a43cdfd3853a5d1408e4f48a8bf268b10450
Philipp Weiermann <philipp.weiermann@bluewin.ch>
2021-03-17 13:40:12 +0100

    Add API-key generation to user generation

--- Improbability/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Improbability/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Improbability.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -53,7 +54,7 @@ namespace Improbability.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, ApiKey = _userManager.GenerateNewAuthenticatorKey() };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
