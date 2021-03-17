5385e18a79d3bb3a7a3496aa631bcf4071e6eb3f
Philipp Weiermann <philipp.weiermann@bluewin.ch>
2021-03-17 16:59:15 +0100

    Add tests in RandomItem tests for GET and PUT

--- ImprobabilityTests/HttpTestsRandomItemsControllerV1.cs
+++ ImprobabilityTests/HttpTestsRandomItemsControllerV1.cs
@@ -1,12 +1,155 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using Xunit;
 
 namespace ImprobabilityTests
 {
     public class HttpTestsRandomItemsControllerV1
     {
-        [Fact]
-        public void Test1()
+        private const string Port = "45347";
+        private const string Root = "http://localhost:" + Port + "/api/v1/randomitems/";
+        private const string KeyUbarcturus = "5RE23H4JHQA2DVLVSEZ525UCRLWXUKGQ";
+        private const string KeyPhilipp = "ZTVWQSDYXSF2UB6B46LKSIGA7GVWHZAQ";
+
+        private static (HttpStatusCode httpStatusCode, string reponseContent) HttpGet(string itemId, string key)
         {
+            var requestUri = new Uri($"{Root}{itemId}");
+            using var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Key", key);
+            var response = httpClient.GetAsync(requestUri).Result;
+            var reponseContent = response.Content.ReadAsStringAsync().Result;
+            return (response.StatusCode, reponseContent);
         }
+
+        private static (HttpStatusCode httpStatusCode, string reponseContent) HttpPut(string itemId, string key, string item)
+        {
+            var requestUri = new Uri($"{Root}{itemId}");
+            using var httpClient = new HttpClient();
+            using var requestContent = new StringContent(item, Encoding.UTF8, "application/json");
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Key", key);
+            var response = httpClient.PutAsync(requestUri, requestContent).Result;
+            var reponseContent = response.Content.ReadAsStringAsync().Result;
+            return (response.StatusCode, reponseContent);
+        }
+
+        private static (HttpStatusCode httpStatusCode, string reponseContent) HttpPost(string key, string items)
+        {
+            var requestUri = new Uri($"{Root}");
+            using var httpClient = new HttpClient();
+            using var requestContent = new StringContent(items, Encoding.UTF8, "application/json");
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Key", key);
+            var response = httpClient.PostAsync(requestUri, requestContent).Result;
+            var reponseContent = response.Content.ReadAsStringAsync().Result;
+            return (response.StatusCode, reponseContent);
+        }
+
+        private static (HttpStatusCode httpStatusCode, string reponseContent) HttpDelete(string itemId, string key)
+        {
+            var requestUri = new Uri($"{Root}{itemId}");
+            using var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Key", key);
+            var response = httpClient.DeleteAsync(requestUri).Result;
+            var reponseContent = response.Content.ReadAsStringAsync().Result;
+            return (response.StatusCode, reponseContent);
+        }
+
+        [Fact]
+        public void OneEqualsOne()
+        {
+            // is the test framework working?
+            Assert.Equal(1, 1);
+        }
+
+        #region GET
+
+        [Fact]
+        public void GetItems()
+        {
+            var (httpStatusCode, _) = HttpGet("", KeyUbarcturus);
+            Assert.Equal(HttpStatusCode.OK, httpStatusCode);
+        }
+
+        [Fact]
+        public void GetItemsWithoutApiKey()
+        {
+            var (httpStatusCode, _) = HttpGet("", "");
+            Assert.Equal(HttpStatusCode.Unauthorized, httpStatusCode);
+        }
+
+        [Fact]
+        public void GetItemsWithFalseApiKey()
+        {
+            var (httpStatusCode, _) = HttpGet("", "ThisKeyIsSupposedDoNotExist");
+            Assert.Equal(HttpStatusCode.Unauthorized, httpStatusCode);
+        }
+
+        [Fact]
+        public void GetOneItem()
+        {
+            var (httpStatusCode, _) = HttpGet("1", KeyUbarcturus);
+            Assert.Equal(HttpStatusCode.OK, httpStatusCode);
+        }
+
+        [Fact]
+        public void GetItemFromOtherUser()
+        {
+            var (httpStatusCode, _) = HttpGet("1", KeyPhilipp);
+            Assert.Equal(HttpStatusCode.Unauthorized, httpStatusCode);
+        }
+
+        [Fact]
+        public void GetItemWithIdWhichNotExists()
+        {
+            var (httpStatusCode, _) = HttpGet("2147483647", KeyPhilipp);
+            Assert.Equal(HttpStatusCode.NotFound, httpStatusCode);
+        }
+
+        [Fact]
+        public void GetItemWithToBigId()
+        {
+            var (httpStatusCode, _) = HttpGet("9999999999", KeyPhilipp);
+            Assert.Equal(HttpStatusCode.BadRequest, httpStatusCode);
+        }
+
+        [Fact]
+        public void GetItemWithStringId()
+        {
+            var (httpStatusCode, _) = HttpGet("abc", KeyPhilipp);
+            Assert.Equal(HttpStatusCode.BadRequest, httpStatusCode);
+        }
+
+        #endregion GET
+
+        #region PUT
+
+        [Fact]
+        public void UpdateOneItem()
+        {
+            const string item = "{\"id\":1,\"name\":\"W10\",\"numberOfPossibleResults\":10,\"description\":\"PUT Seiten\"}";
+            var (httpStatusCode, body) = HttpPut("1", KeyUbarcturus, item);
+            Assert.Equal(HttpStatusCode.OK, httpStatusCode);
+            Assert.Equal(item, body);
+        }
+
+        [Fact]
+        public void UpdateWithDifferentIds()
+        {
+            const string item = "{\"id\":1,\"name\":\"W10\",\"numberOfPossibleResults\":10,\"description\":\"PUT Seiten\"}";
+            var (httpStatusCode, _) = HttpPut("2", KeyUbarcturus, item);
+            Assert.Equal(HttpStatusCode.BadRequest, httpStatusCode);
+        }
+
+        [Fact]
+        public void UpdateItemWithIdWhichNotExists()
+        {
+            const string item = "{\"id\":2147483647,\"name\":\"W10\",\"numberOfPossibleResults\":10,\"description\":\"PUT Seiten\"}";
+            var (httpStatusCode, _) = HttpPut("2147483647", KeyUbarcturus, item);
+            Assert.Equal(HttpStatusCode.NotFound, httpStatusCode);
+        }
+
+        #endregion PUT
     }
 }
