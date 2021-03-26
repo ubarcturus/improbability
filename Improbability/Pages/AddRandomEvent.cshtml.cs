eb3d5c0c8f76964a78c51ef63f754d7040b32185
Philipp Weiermann <philipp.weiermann@bluewin.ch>
2021-03-26 10:31:22 +0100

    Add success message to AddRandomEvent page

--- Improbability/Pages/AddRandomEvent.cshtml.cs
+++ Improbability/Pages/AddRandomEvent.cshtml.cs
@@ -1,42 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Improbability.Data;
 using Improbability.Models;
-using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Net.Http.Json;
-using Microsoft.VisualBasic;
-using System.Collections.ObjectModel;
 
 namespace Improbability.Pages
 {
     public class AddRandomEventModel : PageModel
     {
-        private readonly Improbability.Data.ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
-        public AddRandomEventModel(Improbability.Data.ApplicationDbContext context)
+        public AddRandomEventModel(ApplicationDbContext context)
         {
             _context = context;
         }
 
+        [BindProperty] public RandomEvent RandomEvent { get; set; }
+
+        [BindProperty] public ApplicationUser ApplicationUser { get; init; }
+
         public IActionResult OnGet()
         {
-            // return RedirectToPage("./AddRandomEvent");
             return Page();
         }
 
-        [BindProperty]
-        public RandomEvent RandomEvent { get; set; }
-        [BindProperty] public ApplicationUser ApplicationUser { get; set; }
-
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
@@ -50,11 +45,10 @@ namespace Improbability.Pages
             using var requestContent = new StringContent($"[{JsonConvert.SerializeObject(RandomEvent)}]", Encoding.UTF8, "application/json");
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Key", ApplicationUser.ApiKey);
             var response = await httpClient.PostAsync(requestUri, requestContent);
-            var responseContent = response.Content.ReadAsStringAsync().Result;
 
             if (response.IsSuccessStatusCode)
             {
-                RandomEvent = response.Content.ReadFromJsonAsync<Collection<RandomEvent>>().Result.First();
+                RandomEvent = response.Content.ReadFromJsonAsync<Collection<RandomEvent>>().Result!.First();
                 var page = Page();
                 page.StatusCode = 201;
                 return page;
